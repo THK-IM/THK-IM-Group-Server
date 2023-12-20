8ab781120ad2a1d60cb3cd0d1b9dc0f36b938294
vizoss <think220216@gmail.com>
2023-12-20 22:00:14 +0800

    update sql

--- sql/group_join_apply.sql
+++ sql/group_join_apply.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `group_join_apply_%s`
     `channel`        INT     NOT NULL COMMENT '渠道:1群号,2群二维码,3邀请,4分享',
     `content`        TEXT    NOT NULL COMMENT '申请内容',
     `type`           TINYINT NOT NULL COMMENT '类型 1 申请进群，2邀请进群',
-    `u_ids`          BIGINT  NOT NULL COMMENT '进群用户id多个id#号隔开',
+    `u_ids`          TEXT    NOT NULL COMMENT '进群用户id多个id#号隔开',
     `review_user_id` BIGINT COMMENT '审核用户id',
     `status`         INT     NOT NULL DEFAULT 0 COMMENT '申请状态 0 申请中，1 拒绝，2 通过',
     `update_time`    BIGINT  NOT NULL DEFAULT 0 COMMENT '创建时间 毫秒',
