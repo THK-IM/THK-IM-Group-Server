14f06e350646ada48e4c02e23fbcf96c596a7e91
vizoss <think220216@gmail.com>
2024-02-04 22:38:04 +0800

    update sql

--- sql/group.sql
+++ sql/group.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `group_%s`
 (
-    `id`           BIGINT       NOT NULL COMMENT '群id',
+    `id`           BIGINT       PRIMARY KEY NOT NULL COMMENT '群id',
     `display_id`   varchar(20)  NOT NULL COMMENT '显示id',
     `session_id`   BIGINT       NOT NULL COMMENT '会话id',
     `owner_id`     BIGINT       NOT NULL COMMENT '群主',
@@ -13,6 +13,5 @@ CREATE TABLE IF NOT EXISTS `group_%s`
     `enter_flag`   INT          NOT NULL default 0 COMMENT '进群条件，0 扫码或通过群id随意进群，1 申请通过后可以进入 2 管理员邀请 ',
     `update_time`  BIGINT       NOT NULL DEFAULT 0 COMMENT '创建时间 毫秒',
     `create_time`  BIGINT       NOT NULL DEFAULT 0 COMMENT '创建时间 毫秒',
-    `deleted`      TINYINT      NOT NULL DEFAULT 0 COMMENT '是否删除',
-    UNIQUE INDEX `Group_IDX` (`id`)
+    `deleted`      TINYINT      NOT NULL DEFAULT 0 COMMENT '是否删除'
 );
--- sql/group_display.sql
+++ sql/group_display.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS `group_display_%s`
 (
-    `display_id`  varchar(20) NOT NULL COMMENT '显示id',
+    `display_id`  varchar(20) PRIMARY KEY  NOT NULL COMMENT '显示id',
     `id`          BIGINT      NOT NULL COMMENT 'id',
-    `deleted`      TINYINT      NOT NULL DEFAULT 0 COMMENT '是否删除',
-    UNIQUE INDEX `User_Id_Display_IDX` (`display_id`)
+    `deleted`      TINYINT      NOT NULL DEFAULT 0 COMMENT '是否删除'
 );
